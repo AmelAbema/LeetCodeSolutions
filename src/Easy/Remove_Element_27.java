a1ad317482638a49abd8089e6df6c7c71aac7a64
AmelAbema <dogormous14@gmail.com>
2023-05-07 22:43:50 +0200

    --

---
+++ src/Easy/Remove_Element_27.java
@@ -0,0 +1,16 @@
+package Easy;
+
+public class Remove_Element_27 {
+    public static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                int temp = nums[count];
+                nums[count] = nums[i];
+                nums[i] = temp;
+                count++;
+            }
+        }
+        return count;
+    }
+}
